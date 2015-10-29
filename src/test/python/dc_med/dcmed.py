596ecd2095a77b7bcac5f6852140c0e82c3eb720
grafft <sanekpanov@gmail.com>
2015-10-29 18:59:28 +0300

    [+] Add load data

--- src/test/python/dc_med/dcmed.py
+++ src/test/python/dc_med/dcmed.py
@@ -1,22 +1,30 @@
 __author__ = 'Aleksandr Panov'
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+saved_column_names = []
+saved_category_names = {}
+
 # load data and rename columns
 data = pd.read_csv('real_sample.csv', encoding='utf-8')
 col_len = data.shape[1]
+saved_column_names.extend(list(data.columns))
 data.columns = ['attr_' + str(x) for x in range(col_len)]
 
-# list([data['attr_'+str(x)].dtype for x in range(col_len)])
+# replace '-' to NaN and rename categories to floats
 
-# convert strings to category types
-for i in range(col_len):
-    if data['attr_'+str(i)].dtype == 'O':
-        data['attr_'+str(i)] = data['attr_'+str(i)].astype('category')
+for x in data.columns:
+    column = data[x]
+    if column.dtype == 'O':
+        column = column.astype('category')
 
-# replace '-' to NaN
-for column in [data[x] for x in data.columns]:
-    if hasattr(column, 'cat') and '-' in column.cat.categories:
-        column[column == '-'] = np.nan
-        column.cat.remove_categories('-')
+        if '-' in column.cat.categories:
+            column[column == '-'] = np.nan
+            column = column.cat.remove_categories('-')
+
+        saved_category_names[column.name] = list(column.cat.categories)
+        column = column.cat.rename_categories([str(x) for x in range(len(column.cat.categories))])
+        data[x] = column.astype('float64')
+
