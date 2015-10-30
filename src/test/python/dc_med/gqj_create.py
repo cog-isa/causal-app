44adfb84a3ce1988b525f436d5df66e748ba7549
grafft <sanekpanov@gmail.com>
2015-10-30 18:23:13 +0300

    [+] New gaaq

--- src/test/python/dc_med/gqj_create.py
+++ src/test/python/dc_med/gqj_create.py
@@ -34,7 +34,7 @@ data = data.dropna(subset=[target_column])
 
 f = open('real.gqj', 'w')
 f.write(str(target_column - 1) + '\n' + ','.join(map(str, saved_category_names)) + '\n')
-f.write(';'.join([str(x) + ':' + ','.join(list(data[x].cat.categories)) for x in saved_category_names]) + '\n')
+f.write(';'.join([str(x - 1) + ':' + ','.join(list(data[x].cat.categories)) for x in saved_category_names]) + '\n')
 f.flush()
 
 data.to_csv('real.gqj', sep='\t', na_rep='?', index=False,
