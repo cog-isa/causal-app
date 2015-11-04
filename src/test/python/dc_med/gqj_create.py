60370492b32513d58f18d182571c60d53509d5ce
grafft <sanekpanov@gmail.com>
2015-11-04 12:37:44 +0300

    [~] add new data

--- src/test/python/dc_med/gqj_create.py
+++ src/test/python/dc_med/gqj_create.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+input_file = 'real_sample.csv'
+output_file = 'real.gqj'
 saved_column_names = []
 saved_category_names = {}
 saved_bins = {}
@@ -11,7 +13,7 @@ saved_bins = {}
 target_column = 35
 
 # load data and rename columns
-data = pd.read_csv('real_sample.csv', encoding='utf-8')
+data = pd.read_csv(input_file, encoding='utf-8')
 col_len = data.shape[1]
 saved_column_names.extend(list(data.columns))
 data.columns = [x for x in range(col_len)]
@@ -32,10 +34,10 @@ for x in data.columns:
 
 data = data.dropna(subset=[target_column])
 
-f = open('real.gqj', 'w')
+f = open(output_file, 'w')
 f.write(str(target_column - 1) + '\n' + ','.join([str(x) for x in saved_category_names]) + '\n')
 f.write(';'.join([str(x) + ':' + ','.join(list(data[x].cat.categories)) for x in saved_category_names]) + '\n')
 f.flush()
 
-data.to_csv('real.gqj', sep='\t', na_rep='?', index=False,
+data.to_csv(output_file, sep='\t', na_rep='?', index=False,
             columns=range(1, col_len), mode='a')
