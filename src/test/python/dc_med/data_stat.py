db723adf9e31f54c76569dcea7ed40ae41d1d0ac
grafft <sanekpanov@gmail.com>
2015-11-11 15:09:17 +0300

    [+] New data file

--- src/test/python/dc_med/data_stat.py
+++ src/test/python/dc_med/data_stat.py
@@ -15,6 +15,28 @@ col_len = data.shape[1]
 saved_column_names.extend(list(data.columns))
 data.columns = [x for x in range(col_len)]
 
+for i in range(len(data[21].values)):
+    value = data[19][i]
+    if value == 'Months':
+        data.loc[i, 21] *= 30
+    elif value == 'Weeks':
+        data.loc[i, 21] *= 7
+    elif value == 'Years':
+        data.loc[i, 21] *= 365
+data = data.drop(19, axis=1)
+
+row_to_drop = []
+for i in range(len(data[34].values)):
+    y = data[34][i]
+    if y == 'Complete response' or y == 'Partial Response':
+        data.loc[i, 34] = 'Success'
+    elif y == 'Progressive Disease' or y == 'Stable Disease' or y =='Stable Desease' or y == 'Death':
+        data.loc[i, 34] = 'Fail'
+    elif y == 'Not applicable' or y == 'Not evaluable':
+        row_to_drop.append(i)
+
+data = data.drop(row_to_drop)
+
 # replace '-' to NaN and rename categories to floats
 
 for x in data.columns:
