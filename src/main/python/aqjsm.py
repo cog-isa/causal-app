efe602b03218eb98726d447c097f01b4feb4299c
grafft <sanekpanov@gmail.com>
2015-12-29 15:13:40 +0300

    [+] jsm norris realize

--- src/main/python/aqjsm.py
+++ src/main/python/aqjsm.py
@@ -1,6 +1,6 @@
 import data.data_loading as dl
 import aq.aq_external as aq
-from jsm.jsm_analysis import FactBase
+from jsm.jsm_analysis import FactBase, search_norris
 import sys
 import argparse
 import logging
@@ -11,13 +11,15 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     argparser.add_argument(dest='datafile')
     argparser.add_argument('-l', '--loglevel', choices=log_levels, default='info')
-    argparser.add_argument('-u', '--univer', default='20')
+    argparser.add_argument('-s', '--reasonsize', default=3)
+    argparser.add_argument('-u', '--univer', default=20)
     args = argparser.parse_args()
 
     logging.basicConfig(level=getattr(logging, args.loglevel.upper()),
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         stream=sys.stdout)
-    max_universe_size = int(args.univer)
+    max_universe_size = args.univer
+    max_reason_length = args.reasonsize
 
     data, class_column = dl.load_data(args.datafile)
 
@@ -29,7 +31,14 @@ if __name__ == "__main__":
 
     logging.info('\n'.join([str(class_descriptions[d]) for d in class_descriptions]))
 
-    fb = FactBase(class_column, '1')
+    class_value = '1'
+    fb = FactBase(class_column, class_value)
     fb.build(data, class_descriptions, max_universe_size)
     fb.clear()
     logging.info(str(fb))
+
+    hypotheses = search_norris(fb)
+    for hyp in hypotheses:
+        if hyp.value.count() <= max_reason_length:
+            reasons = [class_descriptions[class_value].properties[i] for i in range(len(hyp.value)) if hyp.value[i]]
+            logging.info('Reasons for property {0}: {1}'.format(class_value, map(str, reasons)))
